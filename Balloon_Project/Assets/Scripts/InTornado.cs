f5ffd6b3b3eea8b47b595aa5ea6be93a914bf806
VitaliyVoloshin <51855930+VitaliyVoloshin@users.noreply.github.com>
2020-05-07 10:54:58 +0300

    +

---
+++ Balloon_Project/Assets/Scripts/InTornado.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InTornado : MonoBehaviour
+{
+    private float T = 2.0f;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Tornado") Invoke("KillMe", T);
+    }
+    private void KillMe() {
+        Destroy(gameObject);
+    }
+}
---
+++ Balloon_Project/Assets/Scripts/TornadoVortex.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TornadoVortex : MonoBehaviour
+{
+    public float PullSpeed;
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player") {
+            GameObject PullOBJ = other.gameObject;
+
+            Vector3 movement = this.transform.position - PullOBJ.transform.position;
+            movement = PullSpeed * Time.deltaTime * movement.normalized;
+            movement = transform.TransformDirection(movement);
+            PullOBJ.gameObject.GetComponent<CharacterController>().Move(movement);
+            // PullOBJ.transform.position = Vector3.MoveTowards(PullOBJ.transform.position, this.transform.position, PullSpeed * Time.deltaTime);
+        }
+    }
+}
