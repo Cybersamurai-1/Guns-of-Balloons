066bf4938ecabd86a1e017a9d35d1198571f5eee
Quake-4 <super-ivan-ivanenko@ukr.net>
2020-05-08 21:18:34 +0300

    physics + tearing the rope

--- Balloon_Project/Assets/Scripts/FPSInput.cs
+++ Balloon_Project/Assets/Scripts/FPSInput.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class FPSInput : MonoBehaviour
 {
-    public float speed = 2.0f;
-    public float speedLeftRight = 50.0f;
+    public GameObject player;
     public bool WASD = false;
+    public float CurrentMaxHeight = 100f; 
+    public float MaxHeight = 200f;
+    public float MinHeight = 50f;
+
+    private float speed = 2;
+    private float speedLeftRight = 50.0f;
     private CharacterController _charController;
     private void Start()
     {
@@ -14,23 +20,36 @@ public class FPSInput : MonoBehaviour
     }
     void Update()
     {
+        float currentY = player.transform.position.y;
+	float speedUp = 0;
+	if (CurrentMaxHeight - currentY >= 0)
+            speedUp = Convert.ToSingle(Math.Sqrt(CurrentMaxHeight - currentY));
+        else            
+            speedUp = Convert.ToSingle(-Math.Sqrt(-CurrentMaxHeight + currentY));
+
 	float deltaZ = 0;
-        float deltaY = 0;
+        float deltaY = speedUp;
         if (WASD)
         {
-	    deltaZ = Input.GetAxis("Vertical") * speed;
+	    deltaZ = Input.GetAxis("Vertical") * speedUp;
             float deltaX = Input.GetAxis("Horizontal") * speedLeftRight;
             transform.Rotate(0, deltaX * Time.deltaTime, 0);
 	}
 
 
         if (Input.GetKey(KeyCode.Space))
-            deltaY += speed;
+        {
+	    if (CurrentMaxHeight+1 <= MaxHeight)
+                CurrentMaxHeight += 0.3f;
+	}
         if (Input.GetKey(KeyCode.LeftShift))
-            deltaY -= speed;
+	{
+            if (CurrentMaxHeight-1 > MinHeight)
+                CurrentMaxHeight -= 0.3f;
+        }
 
         Vector3 movement = new Vector3(0, deltaY, deltaZ);
-        movement = Vector3.ClampMagnitude(movement, speed);
+        movement = Vector3.ClampMagnitude(movement, Math.Abs(speedUp));
         movement *= Time.deltaTime;
         movement = transform.TransformDirection(movement);
         _charController.Move(movement);
---
+++ Balloon_Project/Assets/Scripts/Start_game.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_game : MonoBehaviour
+{
+    public GameObject knot;
+    public GameObject player;
+    
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+	{
+            player.GetComponent<Wind>().enabled = true;
+	    player.GetComponent<FPSInput>().enabled = true;
+            Destroy(knot);
+	    Destroy(this);
+	}
+    }
+}
