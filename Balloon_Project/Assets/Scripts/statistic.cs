70ae81b15ebf02c4a2f4292dddf4ae81849cc6a3
YarKa03Coder <yaroslav0302001@gmail.com>
2020-05-16 10:44:05 +0300

    Fixed bug(1)

---
+++ Balloon_Project/Assets/Scripts/statistic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class statistic : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (File.Exists("temp_statistics.txt"))
+        {
+            string time, distance;
+            using (var sr = new StreamReader("temp_statistics.txt"))
+            {
+                time = sr.ReadLine(); distance = sr.ReadLine();
+            }
+            File.Delete("temp_statistics.txt");
+            text.text = $"Time: {time}  Distance={distance}";
+        }
+        else { text.text = ""; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
